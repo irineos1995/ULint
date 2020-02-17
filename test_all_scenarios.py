7bb809aa62eb388a41dc2027b1a13599d3543bf5
Eirinaios Theodorou <irineos1995@gmail.com>
2020-02-17 22:12:05 +0000

    some scenarios added

--- test_all_scenarios.py
+++ test_all_scenarios.py
@@ -201,36 +201,44 @@ class TestScenario11(unittest.TestCase):
         errors = rule_composer_class.get_line_number_errors()
         self.assertEqual(list(errors), [])
 
-    def test_memory_footprint_1_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=1)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_3_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=3)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_5_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=5)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_10_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=10)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_20_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=20)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_30_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=30)
-        self.assertEqual([], [])
-
-    def test_memory_footprint_40_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=40)
+    def test_memory_footprint_25_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=25)
         self.assertEqual([], [])
 
     def test_memory_footprint_50_pages(self):
-        rule_composer_class = RuleComposer(threshold=0, train_set='scenarios/scenario_11/vanilla_bootstrap_train_pages', max_training_pages=50)
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=50)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_75_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=75)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_100_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=100)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_125_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=125)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_150_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=150)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_175_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=175)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_200_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=200)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_225_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=225)
+        self.assertEqual([], [])
+
+    def test_memory_footprint_233_pages(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples', max_training_pages=233)
         self.assertEqual([], [])
 
 class TestScenario12(unittest.TestCase):
@@ -243,4 +251,51 @@ class TestScenario12(unittest.TestCase):
         set_of_errors = set(errors)
         print(set_of_errors)
         print(len(set_of_errors))
+        self.assertEqual([], [])
+
+class TestScenario13(unittest.TestCase):
+    def test_real_data_with_w3_rules(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples/', star_depth_threshold=3)
+        rule_composer_class.compare_test_page(test_page='scenarios/scenario_12/coreui-free-bootstrap-admin-template-3-next/src/index.html',
+                                              allow_fine_grain_relations=True,
+                                              ignore_unseen_classes=True,
+                                              include_warnings=True
+                                              )
+
+        errors = rule_composer_class.get_line_number_errors()
+
+        set_of_errors = set(errors)
+        print(set_of_errors)
+        print(len(set_of_errors))
+        self.assertEqual([], [])
+
+class TestScenario14(unittest.TestCase):
+    def test_nn_data_processing(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples/')
+        processed_data = rule_composer_class.process_data(rule_composer_class.construct_fine_relations())
+
+        print(processed_data)
+        self.assertEqual([], [])
+
+    def test_nn_data_processing_to_csv(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples/')
+        processed_data = rule_composer_class.process_data(rule_composer_class.construct_fine_relations())
+        rule_composer_class.write_process_data_to_csv(processed_data, 'processed_data.csv')
+
+        print(processed_data)
+        self.assertEqual([], [])
+
+    def test_nn_errors_processing_to_csv(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples/')
+        rule_composer_class.compare_test_page(test_page='scenarios/scenario_12/coreui-free-bootstrap-admin-template-3-next/src/index.html', allow_fine_grain_relations=True, ignore_unseen_classes=True)
+        rule_composer_class.generate_nn_training_data('working.csv')
+
+    def test_nn_prediction(self):
+        rule_composer_class = RuleComposer(threshold=0, train_set='w3_bootstrap_examples/')
+        rule_composer_class.compare_test_page(test_page='scenarios/scenario_12/coreui-free-bootstrap-admin-template-3-next/src/index.html', allow_fine_grain_relations=True, ignore_unseen_classes=True)
+
+        rule_composer_class.generate_nn_training_data('working.csv')
+        rule_composer_class.train()
+
+        print(rule_composer_class.predict([[128,212,2], [128,2,-1]]))
         self.assertEqual([], [])
